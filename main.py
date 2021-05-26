ca945254f0618e2b58314d703da684d1a3866bb9
ABalbae <alexandru.balbae@dxc.com>
2021-05-26 11:33:55 +0300

    This commit contains the last changes for wger application

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ class WengerApi:
         self.login()
 
     def login(self):
-        data = {"username": {self.username}, "password": {self.password}, "submit": "Login"}
+        data = {"username": {self.username},
+                "password": {self.password},
+                "submit": "Login"}
         url = 'https://wger.de/en/user/login'
         login_get = requests.get(url=url, headers=self.header)
         self.header['X-CSRFToken'] = login_get.cookies["csrftoken"]
-        self.header['Referer'] = 'https://wger.de/en/user/login'
-        session_id = login_get.cookies["sessionid"]
-        csrf_token = login_get.cookies["csrftoken"]
+        self.header['Referer'] = url
+        session_id = login_get.cookies.get("sessionid")
+        csrf_token = login_get.cookies.get("csrftoken")
         cookie_full = f"csrftoken={csrf_token}; sessionid={session_id}"
+
         self.header["Cookie"] = cookie_full
         req = requests.post(url=url, data=data, headers=self.header)
         print("The login post cookies are: ")
@@ -38,76 +41,83 @@ class WengerApi:
         # print("The login get cookies are: ")
         # print(login_get.cookies)
 
-    def get_req(self, object):
-        #get the object for ULR
+    def get_req(self, object, offset=None, limit=None):
+
         g1 = requests.get('https://wger.de/api/v2/')
-        g = requests.get(url=g1.json()[object], headers=self.header)
-        if g.status_code == 200:
-            return g.json()
+        url = g1.json().get(object)
+        if url != None:
+            if offset is None and limit is None:
+                url1 = url
+            else:
+                url1 = url + f"?limit={limit}&offset={offset}"
+
+            g = requests.get(url=url1, headers=self.header)
+            if g.status_code == 200:
+                return g.json()
+            else:
+                return g.status_code, "Couldn't make the request!"
         else:
-            return "Couldn't make the request"
+            return False, 'Invalid url'
+
 
     def post_req(self,object, data):
 
         g1 = requests.get('https://wger.de/api/v2/')
         url1 = g1.json().get(object)
+
         if url1 != None:
             req = requests.post(url=url1,data=data,headers=self.header)
             if req.status_code == 201:
-                return f'The post request for {object} was done succusefuly'
+                return req, f'The post request for {object} was done succusefuly'
             else:
-                return f'Error {req.status_code}'
+                return {req.status_code}, 'Error  {req.status_code}'
         else:
-            return 'Invalid url'
-
+            return False, 'Invalid url'
 
 
     def delete_req(self,object, id):
         g1 = requests.get('https://wger.de/api/v2/')
         url1 = g1.json().get(object)
-        url1 = f"{url1}/{id}"
         if url1 != None:
-            req = requests.delete(url=url1, headers=self.header)
+            url = f"{url1}/{id}"
+            req = requests.delete(url=url, headers=self.header)
+            if req.status_code not in (200,202,204):
+                return req.status_code, f"The delete for id {id} couldn't be performed"
             return req
-
-
-
-
-    def get_ingredients(self, limit=20, offset=0):
-        url = f'https://wger.de/api/v2/ingredient/?limit={limit}&offset={offset}'
-        r = requests.get(url=url)
-        if r.status_code == 200:
-            return r.json()['results']
         else:
-            print("Request Error!")
-
+            return False, "Error! The URL doesn't exists!"
 
 
     def create_weight_entry(self,date,value):
+
         data = {
             "date":date,
             "weight":value,
         }
+
         return self.post_req('weightentry',data)
 
 
     def create_nutrition_plan(self,description=None):
-        my_date = date.today()
+
+
         if description == None:
-
+            my_date = date.today()
             description = calendar.day_name[my_date.weekday()]
-
         data = {
             'description': description
         }
         return self.post_req('nutritionplan', data)
 
 
+
     def create_meals_for_nutrition_plans(self,plan):
+
         data = {
             'plan': plan
         }
         return self.post_req('meal',data)
+
 
     def add_meal_item(self,meal,ingredient,amount):
 
@@ -134,12 +144,14 @@ class WengerApi:
 
         workouts = self.get_req('workout')
         list_of_workout = list()
-        for workout in workouts["results"]:
-            list_of_workout.append(workout["id"])
+        workouts_list = workouts.get('results')
+        if workouts_list != None:
+
+            for workout in workouts_list:
+                list_of_workout.append(workout["id"])
 
         if training not in list_of_workout:
-            return f"The training with id {training} doesn't exists!"
-
+            return False, f"The training with id {training} doesn't exists!"
         data ={
 
             "training":training,
@@ -150,17 +162,19 @@ class WengerApi:
 
 
     def add_exercise(self,exerciseday,sets,order):
+
         data = {
-            'exerciseday':exerciseday,
-            'sets':sets,
-            'order':order
+            'exerciseday': exerciseday,
+            'sets': sets,
+            'order': order
         }
 
         return self.post_req('set',data)
 
-    def setting_exercise_set(self, exercise,repetition_unit,reps,weight,weight_unit,rir):
+    def setting_exercise_set(self, set, exercise,repetition_unit,reps,weight,weight_unit,rir):
 
         data = {
+            'set':set,
             'exercise':exercise,
             'repetition_unit':repetition_unit,
             'reps':reps,
@@ -192,53 +206,89 @@ class WengerApi:
         return self.post_req('schedulestep',data)
 
 
-    def delete_workout(self, id):
+    def delete_workout(self, id=None):
         workouts = self.get_req('workout')
         list_of_id = list()
         for workout in workouts['results']:
             list_of_id.append(workout['id'])
-
-        if id in list_of_id:
-            self.delete_req('workout',id)
-            return f"Workout with id {id} was deleted successfully "
+        if id is not None:
+            if id in list_of_id:
+                req = self.delete_req('workout',id)
+                return req, f"Workout with id {id} was deleted successfully "
+            else:
+                return False, f"Workout with id {id} was not found!"
         else:
-            return f"Workout with id {id} was not found!"
+            if len(list_of_id) != 0:
+                for id1 in list_of_id:
+                    self.delete_req('workout',id1)
+
+                return True
+            else:
+                return False, "There are no workouts to be deteled"
 
 
 
-    def delete_exercise(self,workout_id,day_id,exercise_id):
-        workouts = self.get_req('workout')
-        days = self.get_req('day')
-        exercises = self.get_req('set')
-        list_of_workout = list()
-        list_of_days = list()
-        list_of_exercises = list()
+    def delete_exercise(self,workout_id=None,day_id=None,exercise_id=None):
+        if (exercise_id is not None) and (day_id is not None) and (workout_id is not None):
 
-        for workout in workouts["results"]:
-            list_of_workout.append(workout["id"])
-        if workout_id not in list_of_workout:
-            return f"The workout with id {workout_id} couldn't be found"
+            workouts = self.get_req('workout')
+            days = self.get_req('day')
+            exercises = self.get_req('set')
+            list_of_workout = list()
+            list_of_days = list()
+            list_of_exercises = list()
+            for workout in workouts.get("results"):
+                list_of_workout.append(workout.get("id"))
+            if workout_id not in list_of_workout:
+                return False, f"The workout with id {workout_id} couldn't be found"
 
-        for day in days["results"]:
-            if day["training"] == workout_id:
-                list_of_days.append(day["id"])
+            for day in days["results"]:
+                if day.get("training") == workout_id:
+                    list_of_days.append(day.get("id"))
+            if day_id not in list_of_days:
+                return False, f"The day in workout with id {workout_id} and id {day_id} doesn't exists"
 
-        if day_id not in list_of_days:
-            return f"The day in workout with id {workout_id} and id {day_id} doesn't exists"
+            for exercise in exercises.get("results"):
+                if exercise.get("exerciseday") == day_id:
+                    list_of_exercises.append(exercise.get("id"))
 
-        for exercise in exercises["results"]:
-            if exercise["exerciseday"] == day_id:
-                list_of_exercises.append(exercise["id"])
+            if exercise_id not in list_of_exercises:
+                return f"The exercise {exercise_id} in day {day_id} doesn't exists"
+            del_req = self.delete_req('set', exercise_id)
+            return del_req, f"Exercise with {exercise_id} was deleted succesfully!"
 
-        print(list_of_workout)
-        print(list_of_days)
-        print(list_of_exercises)
+        elif (exercise_id is  None) and (day_id is not None) and (workout_id is not None):
+            workouts = self.get_req('workout')
+            days = self.get_req('day')
+            list_of_workouts = list()
+            list_of_days = list()
+            for workout in workouts.get('results'):
+                list_of_workouts.append(workout.get('id'))
+            if workout_id not in list_of_workouts:
+                return False,f"The workout with ID {workout_id} doesn't exists!"
 
-        if exercise_id not in list_of_exercises:
-            return f"The exercise {exercise_id} in day {day_id} doesn't exists"
+            for day in days.get('results'):
+                if day.get('training') == workout_id:
+                    list_of_days.append(day.get('id'))
+            if day_id not in list_of_days:
+                return False, f"The day in workout with id {workout_id} and id {day_id} doesn't exists"
 
-        self.delete_req('set', exercise_id)
-        return "Exercise was deleted succesfully!!!!!"
+            del_req = self.delete_req('day',day_id)
+            return del_req, f"Day with {day_id} was deleted succesfully!"
+
+
+        elif (exercise_id is  None) and (day_id is None) and (workout_id is not None):
+            workouts = self.get_req('workout')
+            list_of_workouts = list()
+            for workout in workouts.get('results'):
+                list_of_workouts.append(workout.get('id'))
+            if workout_id not in list_of_workouts:
+                return False, f"The workout with ID {workout_id} doesn't exists!"
+            del_req = self.delete_workout(workout_id)
+            return del_req, f"Workout with {workout_id} was deteled successfully"
+
+        elif (exercise_id is None) and (day_id is None) and (workout_id is None):
+            return self.delete_workout()
 
 
 
@@ -307,6 +357,8 @@ class WengerApi:
                             amount_req = elem['amount']
                             ingredient_req = elem['ingredient']
                             self.add_meal_item(meal_req,ingredient_req,amount_req)
+
+
 
 
 
--- test1.py
+++ test1.py
@@ -1,88 +1,188 @@
+import random
+
 from main import WengerApi
-
-
-login2 = WengerApi("bbadescu", "TryingVPN!", "Token 3a3e4cfc386b8e7c0ea6e98a922b2bda7866ff94")
+from pprint import pprint
+login1 = WengerApi("balbaealexandru@gmail.com","12345678abc","Token 5c4f8bef9dd4e6d72f0868971ac142cca0157431")
 
 #ex1
-
-# print("Create weight entry")
-# login2.create_weight_entry('2021-05-31','150')
-# print("Get weight entry")
-# weight_list = login2.get_req('weightentry')
-# print(weight_list)
+print("Create weight entry")
+login1.create_weight_entry('2021-05-31','150')
+print("Get weight entry")
+weight_list = login1.get_req('weightentry')
 
 
 #ex2
 
-# for i in range(7):
-#     req1 = login2.create_nutrition_plan()
-#     print(req1)
-#
-# req1 = login2.get_req('nutritionplan')
-#
-# for nutrition in req1["results"][0:3]:
-#
-#     for i in range(5):
-#         req2 = login2.create_meals_for_nutrition_plans(nutrition['id'])
-#         print(req2)
-#
-#
-# for nutrition in req1["results"][3:7]:
-#     for i in range(5):
-#         req2 = login2.create_meals_for_nutrition_plans(nutrition['id'])
-#         print(req2)
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+for day in days_of_week:
+    req1 = login1.create_nutrition_plan(day)
 
 
-#ex3
+nutrition_plans = login1.get_req('nutritionplan')
 
+for nutrition in nutrition_plans.get("results"):
+    req2 = login1.create_meals_for_nutrition_plans(nutrition.get('id'))
+    print(req2)
 
-#ex4
-name_of_workout = "Workout5"
-add_workout = login2.add_workout_day(name_of_workout,"This is workout5")
+list_of_total_meals = list()
+limit = 20
+meals1 = login1.get_req('meal',0,20)
+print(meals1)
+meals2 = login1.get_req('meal',20,20)
+print(meals2)
+for meal in meals1.get('results'):
+    list_of_total_meals.append(meal)
+for meal in meals2.get('results'):
+    list_of_total_meals.append(meal)
+
+print(list_of_total_meals)
+
+for nutrition in nutrition_plans.get('results'):
+
+    for meal in list_of_total_meals:
+        if meal.get('plan') == nutrition.get('id') and nutrition.get('description') in ['Monday', 'Tuesday', 'Wednesday']:
+            kcal = 0
+            while True:
+                amount = 100
+                ingredients_req = login1.get_req('ingredient')
+                list_of_ingredients = ingredients_req.get('results')
+                ingredient = random.choice(list_of_ingredients)
+                print(ingredient)
+                kcal += ingredient.get('energy') * (amount/100)
+                if kcal > 1750:
+                    print(kcal)
+                    break
+                add_item = login1.add_meal_item(meal.get('id'),ingredient.get('id'),amount)
+                print(add_item)
+
+        elif meal.get('plan') == nutrition.get('id') and nutrition.get('description') in ['Thursday', 'Friday', 'Saturday', 'Sunday']:
+            kcal= 0
+            while True:
+                amount = 100
+                ingredients_req = login1.get_req('ingredient')
+                list_of_ingredients = ingredients_req.get('results')
+                ingredient = random.choice(list_of_ingredients)
+                print(ingredient)
+                add_item = login1.add_meal_item(meal.get('id'), ingredient.get('id'),amount)
+                print(add_item)
+                kcal += ingredient.get('energy') * (amount/100)
+                if kcal > 1900:
+                    print(kcal)
+                    break
+
+# ex4
+name_of_workout = "Workout38"
+add_workout = login1.add_workout_day(name_of_workout,"This is workout10")
 print(add_workout)
-# create days for the added workout
 
-get_workout = login2.get_req('workout')
-for workout in get_workout['results']:
-    if workout['name'] == name_of_workout:
-        id1 = workout['id']
-        add_day1 = login2.add_day(workout['id'],"Push",1)
-        add_day2 = login2.add_day(workout['id'],"Pull",2)
-        add_day3 = login2.add_day(workout['id'],"Legs",3)
+exercises_get = login1.get_req('exercise')
+limit = exercises_get.get('count')
+exercises_get_total = login1.get_req('exercise',0,limit)
+list_of_exercises = exercises_get_total.get('results')
 
-        days = login2.get_req('day')
-        list_of_days = list()
-        for day in days["results"]:
-            if day["training"] == id1:
-                list_of_days.append(day["id"])
+chest_exercises_list = list()
+back_exercises_list = list()
+legs_exercises_list = list()
+abs_exercises_list = list()
+arms_exercises_list = list()
+sholders_exercises_list = list()
+calves_exercises_list = list()
 
-        for id in list_of_days:
-            for i in range(6):
-                add_exercise_per_day = login2.add_exercise(id,5,2)
+for exercise in list_of_exercises:
+    if exercise.get('category') == 11:
+        chest_exercises_list.append(exercise)
+    elif exercise.get('category') == 12:
+        back_exercises_list.append(exercise)
+    elif exercise.get('category') == 9:
+        legs_exercises_list.append(exercise)
+    elif exercise.get('category') == 13:
+        sholders_exercises_list.append(exercise)
+    elif exercise.get('category') == 8:
+        arms_exercises_list.append(exercise)
+    elif exercise.get('category') == 14:
+        calves_exercises_list.append(exercise)
+    else:
+        abs_exercises_list.append(exercise)
 
 
+get_workout = login1.get_req('workout')
+for workout in get_workout.get('results'):
+    if workout.get('name') == name_of_workout:
+        id1 = workout.get('id')
+        add_day1 = login1.add_day(id1,"Push1",1)
+        add_day2 = login1.add_day(id1,"Pull1",2)
+        add_day3 = login1.add_day(id1,"Legs1",3)
 
+days = login1.get_req('day',0,200)
+for day in days.get('results'):
+    if day.get('training') == id1:
+        id_day = day.get('id')
+        for i in range(6):
+            number_of_set = 4
+            order = 2
+            req4 = login1.add_exercise(day.get('id'),number_of_set,order)
 
+        list_of_exercises_day = list()
+        if day.get('description') == 'Push1':
+            list_of_exercises_day.append(random.choice(back_exercises_list))
+            list_of_exercises_day.append(random.choice(chest_exercises_list))
+            list_of_exercises_day.append(random.choice(sholders_exercises_list))
+            list_of_exercises_day.append(random.choice(abs_exercises_list))
+            list_of_exercises_day.append(random.choice(chest_exercises_list))
+            list_of_exercises_day.append(random.choice(arms_exercises_list))
+        elif day.get('description') == 'Pull1':
+            list_of_exercises_day.append(random.choice(back_exercises_list))
+            list_of_exercises_day.append(random.choice(back_exercises_list))
+            list_of_exercises_day.append(random.choice(chest_exercises_list))
+            list_of_exercises_day.append(random.choice(abs_exercises_list))
+            list_of_exercises_day.append(random.choice(sholders_exercises_list))
+            list_of_exercises_day.append(random.choice(arms_exercises_list))
+        elif day.get('description') == 'Legs1':
+            list_of_exercises_day.append(random.choice(back_exercises_list))
+            list_of_exercises_day.append(random.choice(legs_exercises_list))
+            list_of_exercises_day.append(random.choice(chest_exercises_list))
+            list_of_exercises_day.append(random.choice(legs_exercises_list))
+            list_of_exercises_day.append(random.choice(calves_exercises_list))
+            list_of_exercises_day.append(random.choice(abs_exercises_list))
+        pprint(list_of_exercises_day)
 
+        exercise_comments = login1.get_req('exercisecomment')
+        comment_limit = exercise_comments.get('count')
+        exercise_images = login1.get_req('exerciseimage')
+        images_limit = exercise_images.get('count')
+        all_exercise_comments = login1.get_req('exercisecomment',offset=0,limit=comment_limit)
+        all_exercise_images = login1.get_req('',offset=0,limit=images_limit)
+        f = open(f"{day.get('description')}_comments.txt","w")
+        for exe in list_of_exercises_day:
+            for comments in all_exercise_comments.get('results'):
+                if exe.get('id') == comments.get('exercise'):
+                    f.write(f"The exercise with id <{exe.get('id')}> has comment <{comments.get('comment')}> \n")
+        f.close()
 
+        sets_req = login1.get_req('set',offset=0,limit=400)
+        list_of_sets = list()
+        for set in sets_req.get('results'):
+            if set.get('exerciseday') == id_day:
+                list_of_sets.append(set)
+        exercise_set_match = zip(list_of_exercises_day,list_of_sets)
+
+        for exercises_match in exercise_set_match:
+            setting_req = login1.setting_exercise_set(exercises_match[1].get('id'),exercises_match[0].get('id'),1,6,"70.00",1,"0.5")
 
 
 #ex 5
-
-# add_schedule = login2.add_schedule("Schedule1","2021-05-21",True,True)
-# schedules = login2.get_req("schedule")
-# for schedule in schedules["results"]:
-#     if schedule["name"] == "Schedule1":
-#         for work in get_workout['results']:
-#             login2.add_workout_to_schedule(schedule["id"],work["id"],duration=5)
-
+add_schedule = login1.add_schedule("Schedule1","2021-05-21",True,True)
+schedules = login1.get_req("schedule")
+for schedule in schedules["results"]:
+    if schedule["name"] == "Schedule1":
+        for work in get_workout['results']:
+            login1.add_workout_to_schedule(schedule["id"],work["id"],duration=5)
 
 #ex6
 
-# delete_exercise = login2.delete_exercise(270637,137838,262858)
-# print(delete_exercise)
+delete_exercise = login1.delete_exercise()
+print(delete_exercise)
 
 #ex7
-# workout_id = get_workout["results"][1]["id"]
-# delete = login2.delete_workout(workout_id)
-# print(delete)
+delete = login1.delete_workout()
+print(delete)
